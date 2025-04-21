0b41675f3029d84d39009d19aa9bb5bd082c4ee7
Olatunde Haroun <haroun.ola@gmail.com>
2025-04-21 22:43:07 +0100

    Implement code changes to enhance functionality and improve performance

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,19 @@
 import Image from "next/image";
+import backgroundImage from "../../public/home/book-bg.png";
 
 export default function Home() {
   return (
-    <div>Home page</div>
+    <div className="flex flex-col relative justify-center items-center h-screen">
+      <Image
+        className="blur-sm"
+        src={backgroundImage}
+        alt="Background Image"
+        fill
+        sizes="100vw"
+        style={{
+          objectFit: "cover"
+        }}
+      />
+    </div>
   );  
 }
