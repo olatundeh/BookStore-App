f22f91b1c9c0db0c119a77223b0d7f5182929926
Olatunde Haroun <haroun.ola@gmail.com>
2025-04-23 23:45:55 +0100

    Add NavLinks component for sidebar navigation

---
+++ src/app/ui/sidebar/NavLinks.js
@@ -0,0 +1,33 @@
+import { HomeIcon, UserGroupIcon, BookOpenIcon, CubeTransparentIcon, RectangleStackIcon, CogIcon, TruckIcon, Squares2X2Icon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
+const links = [
+    {name: 'Home', href: '/', icon: HomeIcon},
+    {name: 'Books', href: '/store', icon: BookOpenIcon},
+    {name: 'Sell or Lend', href: '/stroe/sell-lend', icon: RectangleStackIcon},
+    {name: 'Track Order', href: '/store/track', icon: TruckIcon},
+    {name: 'Community', href: '/store/community', icon: UserGroupIcon},
+    {name: 'Settings', href: '/store/settings', icon: CogIcon},
+    {name: 'About', href: '/store/about', icon: Squares2X2Icon},
+    {name: 'Admin', href: '/store/admin', icon: CubeTransparentIcon},
+];
+
+const NavLinks  = () => {
+    return (
+        <>
+        {
+            links.map((link) => {
+                const IconComponent = link.icon;
+                return (
+                    <Link key={link.name} href={link.href} className='flex h-[48px] grow items-center justify-center gap-2 rounded-md'>
+                        <IconComponent className="w-6" />
+                        <p className="hidden md:block">{link.name}</p>
+                    </Link>
+                )
+            })
+        }
+        </>
+    );
+}
+
+export default NavLinks;
