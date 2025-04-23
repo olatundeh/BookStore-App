10f447070f8b31e4e16a556667b37ad9604efe3a
Olatunde Haroun <haroun.ola@gmail.com>
2025-04-24 00:29:27 +0100

    Refactor Sidebar component layout and styling for improved navigation

--- src/app/ui/sidebar/Sidebar.js
+++ src/app/ui/sidebar/Sidebar.js
@@ -1,19 +1,22 @@
 import Link from 'next/link';
 import NavLinks from './NavLinks';
+import { ArrowLeftCircleIcon, ArrowLeftOnRectangleIcon } from '@heroicons/react/24/solid';
 import Logo from './Logo';
 
 const Sidebar = () => {
     return (
         <div>
-            <Link href="/store" className='bg-purple-500 flex h-20 items-end justify-start rounded-md p-4'>
+            <Link href="/store" className='bg-purple-500 mb-4 flex h-20 items-end justify-start rounded-md p-4'>
                 <div className='w-32 md:w-40'>
                     <Logo/>
                 </div>
             </Link>
-            <NavLinks />
-            <form>
-                <button>Sign Out</button>
-            </form>
+            <div className='flex flex-wrap gap-2 justify-center md:grow flex-row md:justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2'>
+                <NavLinks />
+                <form>
+                    <button className='flex h-[48px] grow items-center justify-center gap-2 rounded-md'> <ArrowLeftOnRectangleIcon className='w-6'/></button>
+                </form>
+            </div>
         </div>
     );
 }
