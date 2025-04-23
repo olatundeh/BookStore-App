edea24e5311060bcc8c00bd42d7faa6a0f6339ee
Olatunde Haroun <haroun.ola@gmail.com>
2025-04-24 00:35:32 +0100

    Fix typo in className for Logo component's div element

--- src/app/ui/sidebar/Logo.js
+++ src/app/ui/sidebar/Logo.js
@@ -2,7 +2,7 @@ import { SparklesIcon } from "@heroicons/react/24/solid";
 
 const Logo = ()=>{
     return (
-        <div className="flex flex-row items-center text-white leading:none">
+        <div className="flex flex-row items-center leading-none  text-white ">
             <SparklesIcon className="w-12 h-12"/>
             <p className="text-[28px] ml-1">BookStore</p>
         </div>
