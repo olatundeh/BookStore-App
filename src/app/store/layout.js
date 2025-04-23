2aa964f8adff0f7e260c211a7f38cfe85e52643f
Olatunde Haroun <haroun.ola@gmail.com>
2025-04-23 23:45:03 +0100

    Add Sidebar component to BookStoreLayout for improved navigation

--- src/app/store/layout.js
+++ src/app/store/layout.js
@@ -1,3 +1,4 @@
+import Sidebar from "../ui/sidebar/Sidebar"
 export const metadata = {
     title: "BookStore - Buy and rent books online",
     description: "An online bookstore where you can buy and rent books.",
@@ -7,6 +8,7 @@ export default function BookStoreLayout({ children }) {
     return (
         <div className="flex xs:flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row justify-center">
             <div className="flex flex-wrap p-2 justify-center">
+                <Sidebar />
                 {children}
             </div>
         </div>
